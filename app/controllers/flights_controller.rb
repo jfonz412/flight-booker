50fba898e55ff28bf60d7a0fd048caa54d66b600
jfonz412 <jfonzone@outlook.com>
2017-06-20 16:28:29 -0400

    find flights method

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,5 +3,17 @@ class FlightsController < ApplicationController
   	@airport_options = Airport.all.map{ |a| a.code }
   	@flight_dates = Flight.all.map{ |f| f.formatted_date }
   	@passenger_max = [1,2,3,4]
+
+  	@available_flights = find_flights
   end
+
+  private
+  	def find_flights
+  		available_flights = []
+  		departures = Flight.all.map do |f|
+  			if f.from_airport.code == params[:departing_from] && f.to_airport.code == params[:arriving_at]
+  				available_flights << f
+  			end
+  		end
+  	end
 end
