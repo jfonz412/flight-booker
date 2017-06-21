1b903ecf97ec5e993e1da491605dbe8eca911b6f
jfonz412 <jfonzone@outlook.com>
2017-06-21 16:02:11 -0400

    nested form for booking and passenger

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,15 @@
 class BookingsController < ApplicationController
   def new
+  	@passengers = []
+  	count = params[:passengers].to_i # number of passengers 1-4
+  	count.times do |n|
+  		@passengers << Passenger.new
+  	end
   	@booking = Booking.new
-  	@passengers = params[:passengers] # number of passengers 1-4
-  	@flight = Flight.find(params[:flight])
+  	@flight = Flight.find(params[:flight_id])
+  end
+
+  def create
+  	redirect_to root_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'flights#index'
+  post '/bookings',  to: 'bookings#create'
   get  '/bookings',  to: 'bookings#new'
   get  '/flights',   to: 'flights#index'
 end
