c16c2f76d22643aab0b6f36d44fd85a0a9ac0914
jfonz412 <jfonzone@outlook.com>
2017-06-21 11:58:21 -0400

    find_flights bugfix

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,6 @@
 class BookingsController < ApplicationController
   def new
+  	@passengers = params[:passengers] # number of passengers 1-4
+  	@flight = Flight.find(params[:flight])
   end
 end
--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,10 @@ class FlightsController < ApplicationController
   	@airport_options = Airport.all.map{ |a| a.code }
   	@flight_dates = Flight.all.map{ |f| f.formatted_date }
   	@passenger_max = [1,2,3,4]
-  	@available_flights = find_flights
+    # unless prevents find_flights from running prematurely
+    unless params[:departing_from].nil?
+  	 @available_flights = find_flights
+    end
   end
 
   private
